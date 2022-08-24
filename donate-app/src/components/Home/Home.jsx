53ddf5b1a7c372df1b5ba647afa2f92bc6b77833
Martyna <martyna.jasinska1@gmail.com>
2022-08-24 12:26:17 +0200

    Home header styles added

--- donate-app/src/components/Home/Home.jsx
+++ donate-app/src/components/Home/Home.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 // import { Link } from "react-router-dom";
-import "./home.styles.scss";
 import HomeHeader from "../HomeHeader/HomeHeader";
 
 const Home = () => {
--- donate-app/src/components/HomeHeader/HomeHeader.jsx
+++ donate-app/src/components/HomeHeader/HomeHeader.jsx
@@ -1,35 +1,64 @@
 import React from "react";
-import { NavLink, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 // import headerimg from "../../assets/Home-Hero-Image.jpg";
-import "./home-header.styles.scss";
+// import "./home-header.styles.scss";
 
 const HomeHeader = () => {
   return (
     <>
       <div className="header">
         <div className="header__img"></div>
-        <div className="container">
-          <Link to="/logowanie">Zaloguj</Link>
-          <Link to="/rejestracja">Załóż konto</Link>
-          <nav>
-            <ul>
-              <li>
-                <NavLink to="start">Start</NavLink>
+        <div className="header__container">
+          <div className="header__top">
+            <Link
+              to="/rejestracja"
+              className="header__signin header__signin-active"
+            >
+              Załóż konto
+            </Link>
+            <Link to="/logowanie" className="header__signin">
+              Zaloguj
+            </Link>
+          </div>
+          <nav className="nav">
+            <ul className="nav__list">
+              <li className="nav__element">
+                <Link to="">Start</Link>
               </li>
-              <li>
-                <NavLink to="start">O co chodzi?</NavLink>
+              <li className="nav__element">
+                <Link to="">O co chodzi?</Link>
               </li>
-              <li>
-                <NavLink to="start">O nas</NavLink>
+              <li className="nav__element">
+                <Link to="">O nas</Link>
               </li>
-              <li>
-                <NavLink to="start">Fundacja i organizacje</NavLink>
+              <li className="nav__element">
+                <Link to="">Fundacja i organizacje</Link>
               </li>
-              <li>
-                <NavLink to="start">Kontakt</NavLink>
+              <li className="nav__element">
+                <Link to="">Kontakt</Link>
               </li>
             </ul>
           </nav>
+          <div className="cta">
+            <h2>
+              Zacznij pomagać! <br />
+              Oddaj niechciane rzeczy w zaufane ręce
+            </h2>
+            <div className="cta__buttons">
+              <button className="cta__btn">
+                <Link to="/logowanie">
+                  Oddaj <br />
+                  rzeczy
+                </Link>
+              </button>
+              <button className="cta__btn">
+                <Link to="/logowanie">
+                  Zorganizuj <br />
+                  zbiórkę
+                </Link>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </>
