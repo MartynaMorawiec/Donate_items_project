1ac67e15f14538a7d4a04b708d0a5a6bfc6efac7
Martyna <martyna.jasinska1@gmail.com>
2022-08-30 13:56:38 +0200

    register component added.

--- donate-app/src/components/Register.jsx
+++ donate-app/src/components/Register.jsx
@@ -1,10 +1,52 @@
 import React from "react";
+import Navigation from "./Navigation.jsx";
+import { ReactComponent as Decoration } from "../assets/Decoration.svg";
 
 const Register = () => {
   return (
-    <>
-      <div>Register</div>
-    </>
+    <section className="login">
+      <Navigation />
+      <div className="login__container">
+        <h2 className="login__title">Zaloguj się</h2>
+        <Decoration />
+        <div className="login__boxes">
+          <div className="login__box">
+            <label htmlFor="login-email" className="login__label">
+              Email
+            </label>
+            <input
+              type="email"
+              className="login__input"
+              id="login-email"
+            ></input>
+          </div>
+          <div className="login__box">
+            <label htmlFor="login-pass" className="login__label">
+              Hasło
+            </label>
+            <input
+              type="password"
+              className="login__input"
+              id="login-pass"
+            ></input>
+          </div>
+          <div className="login__box">
+            <label htmlFor="login-pass-2" className="login__label">
+              Powtrórz hasło
+            </label>
+            <input
+              type="password"
+              className="login__input"
+              id="login-pass-2"
+            ></input>
+          </div>
+        </div>
+        <div className="login__buttons">
+          <button className="login__btn">Załóż konto</button>
+          <button className="login__btn">Zaloguj się</button>
+        </div>
+      </div>
+    </section>
   );
 };
 
